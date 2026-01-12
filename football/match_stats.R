c9ec9f50346114da46d720508724523598f082b1
Manni <manvir.moore@yahoo.co.uk>
2026-01-12 18:15:00 +0000

    getting ready for streamlit dashboard

--- football/match_stats.R
+++ football/match_stats.R
@@ -1,65 +1,55 @@
 library(worldfootballR)
 library(tidyverse)
 
-Prem_2025_players <- fb_league_stats(
+Prem_2025_teams <- fb_season_team_stats(
   country = "ENG",
   gender = "M",
   season_end_year = 2025,
   tier = "1st",
-  non_dom_league_url = NA,
   stat_type = "standard",
-  team_or_player = "player"
+  time_pause = 10
 )
 
-Ligue1_2025_players <- fb_league_stats(
+Ligue1_2025_teams <- fb_league_stats(
   country = "FRA",
   gender = "M",
   season_end_year = 2025,
   tier = "1st",
   non_dom_league_url = NA,
   stat_type = "standard",
-  team_or_player = "player"
+  team_or_player = "team"
 )
 
-LaLiga_2025_players <- fb_league_stats(
+LaLiga_2025_teams <- fb_league_stats(
   country = "ESP",
   gender = "M",
   season_end_year = 2025,
   tier = "1st",
   non_dom_league_url = NA,
   stat_type = "standard",
-  team_or_player = "player"
+  team_or_player = "team"
 )
 
-Bundesliga_2025_players <- fb_league_stats(
+Bundesliga_2025_teams <- fb_league_stats(
   country = "GER",
   gender = "M",
   season_end_year = 2025,
   tier = "1st",
   non_dom_league_url = NA,
   stat_type = "standard",
-  team_or_player = "player"
+  team_or_player = "team"
 )
 
-SerieA_2025_players <- fb_league_stats(
+SerieA_2025_teams <- fb_league_stats(
   country = "ITA",
   gender = "M",
   season_end_year = 2025,
   tier = "1st",
   non_dom_league_url = NA,
   stat_type = "standard",
-  team_or_player = "player"
+  team_or_player = "team"
 )
 
-all_2025 <- rbind(Prem_2025_players, Ligue1_2025_players, Bundesliga_2025_players, LaLiga_2025_players, SerieA_2025_players)
+all_2025 <- rbind(Prem_2025_teams, Ligue1_2025_teams, Bundesliga_2025_teams, LaLiga_2025_teams, SerieA_2025_teams)
 
-write.csv(all_2025, file = "2025_player_stats.csv")
-
-#use this from now on
-
-worldfootballR::load_match_results(
-  country = "ITA",
-  gender = "M",
-  season_end_year = 2025,
-  tier = "1st"
-)
+write.csv(all_2025, file = "2025_team_stats.csv")
